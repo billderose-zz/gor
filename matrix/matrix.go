4cf3edeb3da0b909deb09ad0bbf116337e4fcfaf
Bill DeRose <bill.derose11@gmail.com>
2014-03-21 21:41:01 -0700

    Add matrix package

---
+++ matrix/matrix.go
@@ -0,0 +1,201 @@
+package matrix
+
+import (
+	"fmt"
+	"math"
+)
+
+const epsilon = 1e-12
+
+type row []float64
+
+type Matrix struct {
+	nrow, ncol int
+	Elem       []row
+}
+
+type Pivot struct {
+	Row, Col int
+}
+
+// New returns a 0-matrix with the size initialized
+func New(nrow, ncol int) *Matrix {
+	m := Matrix{nrow: nrow, ncol: ncol, Elem: make([]row, nrow)}
+	for i := 0; i < nrow; i++ {
+		m.Elem[i] = make(row, ncol)
+	}
+	return &m
+}
+
+// NewPivot returns reference to a Pivot on row, col
+func NewPivot(row, col int) *Pivot {
+	return &Pivot{Row: row, Col: col}
+}
+
+// Rank returns the rank of the matrix m
+func (m *Matrix) Rank() int {
+	return int(math.Min(float64(m.nrow), float64(m.ncol)))
+}
+
+// NewFromVector returns a pointer to a matrix populated with entries
+// of the provided vector such that m[i][j] = Elem[i+j]
+func NewFromVector(Elem []float64, nrow int) (m *Matrix) {
+	if nrow < 0 {
+		return nil
+	} else if nrow == 0 {
+		return New(0, 0)
+	}
+	ncol := len(Elem) / nrow
+	m = New(nrow, ncol)
+	for i := 0; i < nrow; i++ {
+		for j := 0; j < ncol; j++ {
+			m.Elem[i][j] = Elem[i*ncol+j]
+		}
+	}
+	return m
+}
+
+// SwapRow swaps row i with row j
+func (m *Matrix) SwapRow(i, j int) *Matrix {
+	m.Elem[i], m.Elem[j] = m.Elem[j], m.Elem[i]
+	return m
+}
+
+// SwapCol swaps colulmn i with column j
+func (m *Matrix) SwapCol(i, j int) *Matrix {
+	return m.Transpose().SwapRow(i, j).Transpose()
+}
+
+// Row returns the ith row from m
+func (m *Matrix) Row(i int) *Matrix {
+	return NewFromVector(m.Elem[i], m.nrow)
+}
+
+// Col returns the ith column from m
+func (m *Matrix) Col(i int) *Matrix {
+	col := make([]float64, 0, m.ncol)
+	for _, row := range m.Elem {
+		col = append(col, row[i])
+	}
+	return NewFromVector(col, m.nrow)
+}
+
+// Transpose returns the transpose of the matrix it is called on
+func (m *Matrix) Transpose() *Matrix {
+	r := make([]row, len(m.Elem[0])) // first row of transpose, first column of m
+	for x := range r {
+		r[x] = make(row, len(m.Elem)) // add columns that are the length m's rows
+	}
+
+	for y, s := range m.Elem {
+		for x, e := range s {
+			r[x][y] = e
+		}
+	}
+	return &Matrix{nrow: m.ncol, ncol: m.nrow, Elem: r}
+}
+
+// FindPivot finds the next element to pivot on in m
+func (m *Matrix) FindPivot() Pivot {
+	rank := m.Rank()
+	for j := 0; j < rank; j++ {
+		for i, row := range m.Elem {
+			if math.Abs(row[j]) > epsilon {
+				if math.Abs(row[j]-1) > epsilon {
+					pivots := 0
+					for k := j - 1; k >= 0; k-- { // scan backwards for pivot in row
+						if math.Abs(row[k]-1) < epsilon {
+							pivots++
+						}
+					}
+					if pivots == 1 { // already a pivoted on this row
+						continue
+					}
+					return Pivot{Row: i, Col: j}
+				} else {
+					if i+1 == m.nrow && j+1 == m.ncol { // corner element; done pivoting
+						continue
+					}
+					zeroed := true
+					for k := i + 1; k < m.nrow; k++ {
+						if math.Abs(m.Elem[k][j]) > epsilon {
+							zeroed = false
+							break
+						}
+					}
+					if !zeroed || i+1 == m.nrow { // rest of column not zeroed or is last row
+						return Pivot{Row: i, Col: j}
+					}
+				}
+			}
+		}
+	}
+	return Pivot{-1, -1}
+}
+
+// Pivot pivots on the Element at index i, j
+func (m *Matrix) Pivot(p Pivot) *Matrix {
+	pivot := m.Elem[p.Row][p.Col]
+	m.ScaleRow(p.Row, 1/pivot) // make pivot entry 1
+	for i, _ := range m.Elem { // for each row
+		if p.Row != i { // skip pivot row
+			if scaleFactor := m.Elem[i][p.Col]; scaleFactor != 0 {
+				m.ScaleRow(p.Row, -scaleFactor)
+				m.AddRow(p.Row, i)
+				m.ScaleRow(p.Row, -1/scaleFactor)
+			}
+		}
+	}
+	return m
+}
+
+// ScaleRow scales row i by factor
+func (m *Matrix) ScaleRow(i int, factor float64) {
+	for n := range m.Elem[i] {
+		m.Elem[i][n] = m.Elem[i][n] * factor
+	}
+}
+
+//AddRow adds row i to row j, stores result in row j
+func (m *Matrix) AddRow(i, j int) {
+	for n := range m.Elem[i] { // for each Element in row
+		m.Elem[j][n] += m.Elem[i][n]
+	}
+}
+
+// RowReduce uses elementary row operations to put m into reduced row form
+func (m *Matrix) RowReduce() {
+	rank := m.Rank()
+	for i := 0; i < rank; i++ {
+		if p := m.FindPivot(); p.Col != -1 {
+			m.Pivot(p)
+		} else {
+			break
+		}
+	}
+	row := 0
+	for i := range m.Elem { // clean up (you really don't want this for large matrices!)
+		for j := range m.Elem[i] {
+			if m.Elem[i][j] == 1 {
+				m.SwapRow(row, i)
+				row++
+			}
+			if math.Abs(m.Elem[i][j]) < epsilon {
+				m.Elem[i][j] = 0.0
+			}
+		}
+	}
+}
+
+// String returns the string representation of a Matrix
+func (m Matrix) String() (s string) {
+	s += fmt.Sprintf("%d-by-%d matrix\n", m.nrow, m.ncol)
+	for i, row := range m.Elem {
+		if i == 0 {
+			s += fmt.Sprint(row)
+		} else {
+			s += "\n" + fmt.Sprint(row)
+		}
+	}
+	return s
+}
---
+++ matrix/matrix_test.go
@@ -0,0 +1,163 @@
+package matrix
+
+import (
+	//"math/rand"
+	//"fmt"
+	"math"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	nrow := 5
+	ncol := 6
+	m := New(nrow, ncol)
+	if m.nrow != nrow {
+		t.Errorf("New matrix should have %d", nrow, "rows. Has %d", m.nrow)
+	}
+	if m.ncol != ncol {
+		t.Errorf("New matrix should have %d", ncol, "rows. Has %d", m.ncol)
+	}
+	for i := 0; i < nrow; i++ {
+		for j := 0; j < ncol; j++ {
+			if m.Elem[i][j] != 0 {
+				t.Errorf("New matrix not iniitalized to all 0")
+			}
+		}
+	}
+}
+
+func TestNewFromVector(t *testing.T) {
+	vec := []float64{1, 2, 3, 4, 5, 6}
+	nrow := 3
+	ncol := 2
+	m := NewFromVector(vec, nrow)
+	for i := 0; i < nrow; i++ {
+		for j := 0; j < ncol; j++ {
+			if m.Elem[i][j] != vec[i*ncol+j] {
+				t.Errorf("New matrix not iniitalized with correct entries."+
+					"Entry %f,%f should be %f, but is %f.", i, j, vec[i*ncol+j], m.Elem[i][j])
+			}
+		}
+	}
+
+	m = NewFromVector(vec, -1)
+	if m != nil {
+		t.Errorf("NewFromVector expected to return nil matrix when provided with negative nrow argument")
+	}
+
+	m = NewFromVector(vec, 0)
+	if m.nrow != 0 {
+		t.Errorf("NewFromVector expected to return 0-by-0 matrix when provided with nrow = 0. Instead, "+
+			"produced matrix %d-by-%d", m.nrow, m.ncol)
+	}
+	if m.ncol != 0 {
+		t.Errorf("NewFromVector expected to return 0-by-0 matrix when provided with nrow = 0. Instead, "+
+			"produced matrix %d-by-%d", m.nrow, m.ncol)
+	}
+}
+
+func TestSwapRow(t *testing.T) {
+	vec := []float64{1, 2, 3, 4, 5, 6}
+	nrow := 3
+	//ncol := 2
+	m := NewFromVector(vec, nrow)
+	m.SwapRow(1, 2)
+	if m.Elem[1][0] != 5 || m.Elem[1][1] != 6 {
+		t.Errorf("SwapRow incorrectly exchanging rows")
+	}
+	if m.Elem[2][0] != 3 || m.Elem[2][1] != 4 {
+		t.Errorf("SwapRow incorrectly exchanging rows")
+	}
+}
+
+func TestSwapCol(t *testing.T) {
+	vec := []float64{1, 2, 3, 4, 5, 6}
+	nrow := 3
+	//ncol := 2
+	m := NewFromVector(vec, nrow)
+	m = m.SwapCol(0, 1) // this is an API inconsistency. Library should handle all memory
+	if m.Elem[0][0] != 2 || m.Elem[1][0] != 4 || m.Elem[2][0] != 6 {
+		t.Errorf("SwapCol incorrectly exchanging columns")
+	}
+	if m.Elem[0][1] != 1 || m.Elem[1][1] != 3 || m.Elem[2][1] != 5 {
+		t.Errorf("SwapCol incorrectly exchanging columns")
+	}
+}
+
+var pivotTests = []struct {
+	vec  []float64
+	nrow int
+	pRow int
+	pCol int
+}{
+	{[]float64{0, 0,
+		1, -1,
+		5, 6,
+		7, 9}, 4, 1, 0},
+	{[]float64{4.4, 5,
+		6, 7.5}, 2, 0, 0},
+	{[]float64{1, 9.0 / 10.0, 5.0 / 6.0,
+		0, 5, 3}, 2, 1, 1},
+	{[]float64{-1, 0, 4, 5, 6, 7}, 3, 0, 0},
+	{[]float64{1, 2, 3, 4, 5, 6, 7, 8}, 8, 0, 0},
+	{[]float64{-2, 0, 4, 5, 9.2, 0.3}, 2, 0, 0},
+	{[]float64{0, 0, 0, 1, 9.2, 0}, 2, 1, 0},
+	{[]float64{0, 0, 0, 0, 9.2, 0}, 2, 1, 1},
+	{[]float64{1, 0, 0, 0, 1, 0, 0, 0, 1}, 3, -1, -1},
+	{[]float64{3, 4, 5, 7, 9, 1, 0, 8, 5, 4, 3, -7}, 4, 0, 0},
+	{[]float64{5.5, 6.7, 9, 0.1, 3, 4, 5.6, 8}, 2, 0, 0},
+	{[]float64{0, 6.7, 9, 0.1, 3, 4, 5.6, 8, .3, .5, .6, .11, 3.0 / 4.0, 5.0 / 7.0, 8.0 / 9.0, 0}, 4, 1, 0},
+}
+
+var reducedTests = []struct {
+	vec []float64
+}{
+	{[]float64{1, 0, 0, 1, 0, 0, 0, 0}},
+	{[]float64{1, 0, 0, 1}},
+	{[]float64{1, 0, 0.2933333, 0, 1, 0.6}},
+	{[]float64{1, 0, 0, 1, 0, 0}},
+	{[]float64{1, 0, 0, 0, 0, 0, 0, 0}},
+	{[]float64{1, 0, -2, 0, 1, 1.11957}},
+	{[]float64{1, 9.2, 0, 0, 0, 0}},
+	{[]float64{0, 1, 0, 0, 0, 0}},
+	{[]float64{1, 0, 0, 0, 1, 0, 0, 0, 1}},
+	{[]float64{1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0}},
+	{[]float64{1, 0, -0.8, -28, 0, 1, 2, 23}},
+	{[]float64{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}},
+}
+
+func TestFindPivot(t *testing.T) {
+	for i, test := range pivotTests {
+		m := NewFromVector(test.vec, test.nrow)
+		p := m.FindPivot()
+
+		if p.Col != test.pCol {
+			t.Errorf("Incorrectly finding  pivot column on test case %d. "+
+				"Found %d, should be %d\n", i, p.Col, test.pCol)
+		}
+		if p.Row != test.pRow {
+			t.Errorf("Incorrectly finding  pivot row on test case %d. "+
+				"Found %d, should be %d\n", i, p.Row, test.pRow)
+		}
+	}
+}
+
+func TestRowReduce(t *testing.T) {
+	for i, test := range pivotTests {
+		m := NewFromVector(test.vec, test.nrow)
+		m.RowReduce()
+		check := NewFromVector(reducedTests[i].vec, test.nrow)
+		if check.ncol != m.ncol || check.nrow != m.nrow {
+			t.Error("RowReduce altering the number of rows/columns")
+		}
+		for j := range m.Elem {
+			for k := range m.Elem[j] {
+				if math.Abs(m.Elem[j][k]-check.Elem[j][k]) > 1e-5 {
+					t.Error("Incorrectly row-reducing matrix,"+
+						"should be:\n", check, "\nIs:\n", m)
+				}
+			}
+		}
+
+	}
+}
